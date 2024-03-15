7f00f254d7d463a065a76f3e8690dd9d4a39afe7
Adri Priego <priegolucena@gmail.com>
2024-03-15 10:21:38 +0100

    segundas rutas

---
+++ models/Question.model.js
@@ -0,0 +1,22 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const questionSchema = new Schema(
+{
+    question: {
+        type: String,
+        require: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    car: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Car"
+    }
+}
+)
+
+const Question = model("Question", questionSchema)
+
+module.exports = Question
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+const e = require("express");
 const Car = require("../models/Car.model")
+const Question = require("../models/Question.model") 
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
@@ -8,10 +10,13 @@ router.get("/", (req, res, next) => {
 const authRouter = require("./auth.routes")
 router.use("/auth", authRouter)
 
+const userRouter = require("./user.routes")
+router.use("user",userRouter)
+
 
 router.post("/cars", async (req, res, next) => {
   const { name, model, category, year, cv, km, price, image, userCar } = req.body
-
+  const userId = req.params.userId
 
   try {
     Car.create({
@@ -23,7 +28,7 @@ router.post("/cars", async (req, res, next) => {
       km,
       price,
       image,
-      userCar
+      userCar: userId
     })
 
     res.sendStatus(201)
@@ -34,7 +39,135 @@ router.post("/cars", async (req, res, next) => {
 })
 
 router.get("/cars", async (req, res, next) => {
-  
+  try {
+    const cars = await Car.find()
+    
+    res.status(200).json(cars)
+  } catch (error) {
+    next(error)
+  }
 })
 
+router.get("/cars/:categoryName", async (req, res, next) => {
+  try {
+    const carCategory = await Car.find({category: req.params.categoryName})
+
+    res.status(200).json(carCategory)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get("/cars/:carId", async (req, res, next) => {
+  const carId = req.params.carId
+
+  try {
+    const carDetail = await Car.findById(carId)
+
+    if(carDetail === null) {
+      res.status(404).json({messageError: "coche no encontrado"})
+    }
+
+    res.status(200).json(carDetail)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put("/cars/:carId", async (req, res, next) => {
+  const carId = req.params.carId
+  console.log(carId)
+
+  const { name, model, category, year, cv, km, price, image } = req.body
+
+  try {
+    
+    const response = await Car.findByIdAndUpdate(carId, {
+      name, 
+      model, 
+      category, 
+      year, 
+      cv, 
+      km, 
+      price, 
+      image
+    }, {new: true})
+
+    res.status(202).json(response)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete("/cars/:carId", async (req, res, next) => {
+  const carId = req.params.carId
+
+  try {
+    const response = await Car.findByIdAndDelete(carId)
+    res.status(202).json({message: "coche eliminado"})
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+router.post("/question/:carId", async (req, res, next) => {
+  const {question} = req.body
+  const carId = req.params.carId
+  
+  try {
+    
+     Question.create({
+      question,
+      car: carId
+     })
+
+     res.sendStatus(201)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put("/question/:questionId", async (req, res, next) => {
+  const {question} = req.body
+  const questionId = req.params.questionId
+
+  try {
+    
+    const response = await Question.findByIdAndUpdate(questionId, {
+      question
+    }, {new: true})
+
+    res.status(202).json(response)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete("/question/:questionId", async (req, res, next) => {
+  const questionId = req.params.questionId
+
+  try {
+    
+    await Question.findByIdAndDelete(questionId)
+
+    res.status(202).json({message: "pregunta eliminada"})
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get("/question/:carId", async (req, res, next) => {
+  const carId = req.params.carId
+
+  try {
+    
+    const questions = await Question.find({car: carId})
+
+    res.status(200).json(questions)
+  } catch (error) {
+    next(error)
+  }
+
+}) 
 module.exports = router;
